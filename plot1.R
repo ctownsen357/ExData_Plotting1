190284816bc382029bed7ef4624d9a5dd5a7b338
Chris Townsend <ctownsen357@gmail.com>
2016-03-12 17:57:21 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,2 +1,14 @@
+#exploratory data analysis: course project 1
+#plot1
+#using sqldf library to query the input file like a table (very neat library)
+
 library(sqldf)
+
+#load only the target records
 hhpc <- read.csv.sql("household_power_consumption.txt", sep = ";", sql = "select * from file where Date in ('1/2/2007', '2/2/2007')")
+
+png("plot1.png") #open a png file
+
+hist(as.numeric(hhpc$Global_active_power), col = "red", main = "Global Active Power", ylab = "Frequency", xlab = "Global Active Power (kilowatts)")
+
+dev.off() #turn off the png device
