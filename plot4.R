52b045eaeed09e2d1cf2222eb3077878758a1ff7
Chris Townsend <ctownsen357@gmail.com>
2016-03-12 17:58:38 -0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,3 +1,9 @@
+#exploratory data analysis: course project 1
+#plot4
+#using sqldf library to query the input file like a table (very neat library)
+library(sqldf)
+#load only the target records
+hhpc <- read.csv.sql("household_power_consumption.txt", sep = ";", sql = "select * from file where Date in ('1/2/2007', '2/2/2007')")
 
 
 png(filename = "plot4.png")
